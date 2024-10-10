fe22b6405489f4d49eaceab0bdadb449c3b4cfd4
minsu.lee <minsu.rob@gmail.com>
2024-10-10 17:54:44 +0900

    fix: compile 오류 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,7 +24,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env' : '.env',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').required(),
--- src/restaurants/repositories/category.repository.ts
+++ src/restaurants/repositories/category.repository.ts
@@ -6,7 +6,7 @@ export class CategoryRepository extends Repository<Category> {
   async getOrCreate(name: string): Promise<Category> {
     const categoryName = name.trim().toLowerCase();
     const categorySlug = categoryName.replace(/ /g, '-');
-    let category = await this.findOne({ slug: categorySlug });
+    let category = await this.findOne({ where: { slug: categorySlug } });
     if (!category) {
       category = await this.save(
         this.create({ slug: categorySlug, name: categoryName }),
