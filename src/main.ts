025c0352a97629c9b82a7d64da01401433255a9c
minsu.lee <minsu.rob@gmail.com>
2024-10-18 22:18:34 +0900

    fix: user data 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+var cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(cors());
+
   await app.listen(4000);
 }
 bootstrap();
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -4,11 +4,25 @@ import {
   ObjectType,
   registerEnumType,
 } from '@nestjs/graphql';
-import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  OneToMany,
+  UpdateDateColumn,
+} from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { InternalServerErrorException } from '@nestjs/common';
-import { IsBoolean, IsEmail, IsEnum, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsDate,
+  IsEmail,
+  IsEnum,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 
 export enum UserRole {
@@ -24,31 +38,77 @@ registerEnumType(UserRole, { name: 'UserRole' });
 @Entity()
 export class User extends CoreEntity {
   @Column()
-  @Field(type => String)
+  @Field((type) => String)
   @IsEmail()
   email: string;
 
   @Column({ select: false })
-  @Field(type => String)
+  @Field((type) => String)
   @IsString()
   password: string;
 
   @Column({ type: 'enum', enum: UserRole })
-  @Field(type => UserRole)
+  @Field((type) => UserRole)
   @IsEnum(UserRole)
   role: UserRole;
 
   @Column({ default: false })
-  @Field(type => Boolean)
+  @Field((type) => Boolean)
   @IsBoolean()
   verified: boolean;
 
-  @Field(type => [Restaurant])
-  @OneToMany(
-    type => Restaurant,
-    restaurant => restaurant.owner,
-  )
+  @Field((type) => [Restaurant])
+  @OneToMany((type) => Restaurant, (restaurant) => restaurant.owner)
   restaurants: Restaurant[];
+
+  // climbing data
+
+  @Field((type) => Number)
+  @Column({ default: 170 })
+  @IsNumber()
+  reach: number;
+
+  @Field((type) => Number)
+  @Column({ default: 170 })
+  @IsNumber()
+  height: number;
+
+  @Field((type) => Number, { nullable: true })
+  @Column({ default: 0 })
+  @IsNumber()
+  follower: number;
+
+  @Field((type) => Number, { nullable: true })
+  @Column({ default: 0 })
+  @IsNumber()
+  followering: number;
+
+  @Field((type) => String, { nullable: true })
+  @Column({ default: '' })
+  @IsString()
+  instaId: string;
+
+  @Field((type) => Date, { nullable: true })
+  @IsDate()
+  @UpdateDateColumn()
+  startedAt: Date; // clmbing stared
+
+  @Column({ default: true })
+  @Field((type) => Boolean)
+  @IsBoolean()
+  isAllow: boolean; // auth
+
+  // home zym
+
+  // crew object
+
+  // avatar object
+
+  // @Field((type) => [Follower])
+  // @OneToOne((type) => Follower, (follower) => follower.owner)
+  // follower: Follower[];
+
+  //need following
 
   @BeforeInsert()
   @BeforeUpdate()
